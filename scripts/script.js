13cdc424436d6aa8009c95f72cb9571aaeab04a6
DrAkuzi <79124550+DrAkuzi@users.noreply.github.com>
2022-06-12 00:05:16 +0800

    temp rotation + temp shader
    found a temp lookat logic but not perfect.
    also edited the xray shader

--- scripts/script.js
+++ scripts/script.js
@@ -30,7 +30,7 @@ var Reactive = require('Reactive');
 
 (async function() { // Enables async/await in JS [part 1]
 
-    const camera = await Scene.root.findFirst('Spark'); //'Camera'
+    const camera = await Scene.root.findFirst('Camera'); //'Camera'
     const tracker = await Scene.root.findFirst('pivot0');
     const plane = await Scene.root.findFirst('plane0');
     const toRotateDuplicate = await Scene.root.findFirst('toRotateDuplicate');
@@ -41,17 +41,18 @@ var Reactive = require('Reactive');
         axis_x = axis_x.div(norm);
         axis_y = axis_y.div(norm);
         axis_z = axis_z.div(norm);
-        var angle_radians = angle_degrees.mul(Math.PI).div(180.0);
-        var cos = Reactive.cos(angle_radians.div(2.0));
-        var sin = Reactive.sin(angle_radians.div(2.0));
+        //var angle_radians = angle_degrees.mul(Math.PI).div(180.0);
+        var cos = Reactive.cos(angle_degrees.div(2.0));
+        var sin = Reactive.sin(angle_degrees.div(2.0));
         return Reactive.quaternion(cos, axis_x.mul(sin), axis_y.mul(sin), axis_z.mul(sin));
 
     }
 
+    //method1 - works
     // Get the camera world space position
-    const cameraPositionSignal = tracker.worldTransform.inverse().applyToPoint(camera.worldTransform.position);
+    //const cameraPositionSignal = tracker.worldTransform.inverse().applyToPoint(camera.worldTransform.position);
     // Apply the rotation of the relative transform between plane tracker and camera world position
-    plane.transform.rotation = toRotateDuplicate.transform.lookAt(cameraPositionSignal).rotation.mul(Reactive.quaternion(0, -90.0, 0, -90.0));
+    //plane.transform.rotation = toRotateDuplicate.transform.lookAt(cameraPositionSignal).rotation;
 
     //method 2 - not working
     //const angleAxis = toRotateDuplicate.transform.lookAt(cameraPositionSignal).rotation.angleAxis();
@@ -60,7 +61,12 @@ var Reactive = require('Reactive');
     //method 3 - not working
     //var theAxis = cameraPositionSignal.cross(Reactive.vector(1, 0, 0)).normalize();
     //var theAngle = Reactive.acos(cameraPositionSignal.normalize().dot(Reactive.vector(1, 0, 0)));
+
     //var temp = await axisRotation(theAxis.x, theAxis.y, theAxis.z, theAngle);
     //plane.transform.rotation = temp;
-    //Diagnostics.watch("y: ", temp.x);
+
+    //method 4 - works
+    plane.transform.rotation = toRotateDuplicate.worldTransform.lookAt(camera.worldTransform.position).rotation;
+
+    //Diagnostics.watch("y: ", theAngle.mul(Reactive.val(180.0).div(Math.PI)));
 })(); // Enables async/await in JS [part 2]
