a7586c848597d58a35c99879e62364eac6f57432
DrAkuzi <79124550+DrAkuzi@users.noreply.github.com>
2022-06-11 19:54:45 +0800

    More test on rotation

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,7 @@ const Scene = require('Scene');
 const Patches = require('Patches');
 // Use export keyword to make a symbol available in scripting debug console
 export const Diagnostics = require('Diagnostics');
+var Reactive = require('Reactive');
 
 // To use variables and functions across files, use export/import keyword
 // export const animationDuration = 10;
@@ -29,16 +30,37 @@ export const Diagnostics = require('Diagnostics');
 
 (async function() { // Enables async/await in JS [part 1]
 
-    const camera = await Scene.root.findFirst('Camera');
-    const tracker = await Scene.root.findFirst('planeTracker0');
+    const camera = await Scene.root.findFirst('Spark'); //'Camera'
+    const tracker = await Scene.root.findFirst('pivot0');
     const plane = await Scene.root.findFirst('plane0');
+    const toRotateDuplicate = await Scene.root.findFirst('toRotateDuplicate');
+
+    async function axisRotation(axis_x, axis_y, axis_z, angle_degrees) {
+
+        var norm = Reactive.sqrt(axis_x.mul(axis_x.add(axis_y)).mul(axis_y.add(axis_z)).mul(axis_z));
+        axis_x = axis_x.div(norm);
+        axis_y = axis_y.div(norm);
+        axis_z = axis_z.div(norm);
+        var angle_radians = angle_degrees.mul(Math.PI).div(180.0);
+        var cos = Reactive.cos(angle_radians.div(2.0));
+        var sin = Reactive.sin(angle_radians.div(2.0));
+        return Reactive.quaternion(cos, axis_x.mul(sin), axis_y.mul(sin), axis_z.mul(sin));
+
+    }
 
     // Get the camera world space position
     const cameraPositionSignal = tracker.worldTransform.inverse().applyToPoint(camera.worldTransform.position);
-    await Patches.inputs.setVector('rotate', tracker.transform.lookAt(cameraPositionSignal).rotation);
     // Apply the rotation of the relative transform between plane tracker and camera world position
-    //plane.transform.rotation = tracker.transform.lookAt(cameraPositionSignal).rotation;
+    plane.transform.rotation = toRotateDuplicate.transform.lookAt(cameraPositionSignal).rotation.mul(Reactive.quaternion(0, -90.0, 0, -90.0));
 
-    //Diagnostics.log("here");
+    //method 2 - not working
+    //const angleAxis = toRotateDuplicate.transform.lookAt(cameraPositionSignal).rotation.angleAxis();
+    //plane.transform.rotation = await axisRotation(angleAxis.y, angleAxis.z, angleAxis.w, angleAxis.x);
 
+    //method 3 - not working
+    //var theAxis = cameraPositionSignal.cross(Reactive.vector(1, 0, 0)).normalize();
+    //var theAngle = Reactive.acos(cameraPositionSignal.normalize().dot(Reactive.vector(1, 0, 0)));
+    //var temp = await axisRotation(theAxis.x, theAxis.y, theAxis.z, theAngle);
+    //plane.transform.rotation = temp;
+    //Diagnostics.watch("y: ", temp.x);
 })(); // Enables async/await in JS [part 2]
